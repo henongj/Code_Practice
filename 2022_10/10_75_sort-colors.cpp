6c59f6e86cc8e0eb3818770d2b2b8a5e9dde324f
henongj (DevSheer) <68506070+henongj@users.noreply.github.com>
2022-10-10 21:06:11 +0900

    Create 10_75_sort-colors.cpp

---
+++ 2022_10/10_75_sort-colors.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void swap(int& a, int& b)
+    {
+        int c = a;
+        a = b;
+        b = c;
+    }
+    void sortColors(vector<int>& nums) {
+        int left = 0; int right = nums.size()-1; int idx = 0;
+        
+        while(idx <= right){
+            
+            if( nums[idx] == 2 )
+            {
+                swap(nums[idx] , nums[right]);
+                right--;
+            }
+            else if( nums[idx] == 0)
+            {
+                swap(nums[idx] , nums[left]);
+                idx++; left++;
+            }
+            else
+            {
+                idx++;
+            }
+        }
+        
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            if(i == 0)
+                cout << "[";
+            
+            cout<<nums[i];
+            
+            if( i != nums.size()-1)
+                cout<<",";
+            else{
+                cout <<"]";
+            }                
+        }
+        
+    }
+};
